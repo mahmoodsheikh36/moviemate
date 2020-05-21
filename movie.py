de4cb508f328c418f095bb72016f098784643296
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-05-21 12:55:15 +0300

    add small_poster to Movie class

--- movie.py
+++ movie.py
@@ -1,5 +1,7 @@
 class Movie:
-    def __init__(self, title, year, poster):
+    def __init__(self, title, year, poster, small_poster=None):
         self.title = title
-        self.year = int(year)
+        print(year.split('–')[0])
+        self.year = year.split('–')[0]
         self.poster = poster
+        self.small_poster = small_poster
