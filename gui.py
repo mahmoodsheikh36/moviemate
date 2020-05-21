aababf7da980cd3bfdae7944d3bbdf90fd9831fb
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-05-21 12:56:17 +0300

    improved, refactored the widgets

--- gui.py
+++ gui.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 import gi
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk, GdkPixbuf, Gdk, GLib, Gio
+from gi.repository import Gtk, GdkPixbuf, Gdk, GLib
 import threading
 import requests
 
-from imdb import search
+from data_providers import *
 
 class LibraryWidget(Gtk.FlowBox):
     def __init__(self):
         Gtk.FlowBox.__init__(self)
         self.set_homogeneous(True)
         self.set_row_spacing(10)
-        #self.set_border_width(10)
         self.set_valign(Gtk.Align.START)
         self.set_selection_mode(Gtk.SelectionMode.NONE)
         self.set_max_children_per_line(100)
@@ -43,71 +42,68 @@ class MovieWidget(Gtk.Box):
 
 class Window(Gtk.Window):
     def __init__(self):
-        builder = Gtk.Builder()
-        builder.add_from_file("ui_data/discover_widget.glade")
-
         Gtk.Window.__init__(self, title="moviemate")
         self.set_default_size(1000, 600)
         self.connect("destroy", Gtk.main_quit)
 
         self.notebook = Gtk.Notebook()
         self.notebook.set_tab_pos(Gtk.PositionType.LEFT)
-
-        self.library_widget = LibraryWidget()
-
         self.notebook.append_page(Gtk.Label(label='hey'),
                                   Gtk.Label(label="Library"))
 
-        discover_search = builder.get_object('discover_search')
-        discover_search.connect('search-changed', self.begin_search)
-
-        # self.button.connect("clicked",
-        #                     lambda _ : self.async_add_movies_from_search('endgame'))
         self.add(self.notebook)
 
-        self.notebook.append_page(builder.get_object('discover_widget'),
-                                  Gtk.Label(label='Discover'))
-
-        self.movies_widget = builder.get_object('discover_movies')
+        self.init_discover_widget()
 
         self.move(10, 10)
         Window._style()
         self.show_all()
 
-    def begin_search(self, search_entry):
+    def init_discover_widget(self):
+        builder = Gtk.Builder()
+        builder.add_from_file("ui_data/discover_widget.glade")
+        builder.add_from_file("ui_data/movies_widget.glade")
+        discover_search = builder.get_object('discover_search')
+        discover_search.connect('search-changed', self.discover_search)
+        self.discover_movies_widget = builder.get_object('movies_widget')
+        movies_widget_viewport = builder.get_object('movies_widget_viewport')
+        movies_widget_viewport.add(self.discover_movies_widget)
+        self.notebook.append_page(builder.get_object('discover_widget'),
+                                  Gtk.Label(label='Discover'))
+
+    def discover_search(self, search_entry):
         text = search_entry.get_text()
         if text == '':
             return
-        self.async_add_movies_from_search(text)
+        self.async_add_movies_from_search(text, self.discover_movies_widget)
 
-    def stop_search(self):
-        pass
-
-    def focus_to_result(self):
-        pass
-
-    def async_add_movies_from_search(self, query):
-        thread = threading.Thread(target=self.add_movies_from_search, args=(query,))
+    def async_add_movies_from_search(self, query, movies_widget):
+        thread = threading.Thread(target=self.add_movies_from_search, args=(query,
+                                                                            movies_widget))
         thread.start()
 
-    def add_movies_from_search(self, query):
-        movies = search(query)
+    def add_movies_from_search(self, query, movies_widget):
+        for movie_widget in movies_widget.get_children():
+            self.movies_widget.remove(movie_widget)
+        c = OMDbClient()
+        movies = c.search(query)
         for movie in movies:
-            image_url = movie.poster
+            if movie.small_poster:
+                image_url = movie.small_poster
+            else:
+                image_url = movie.poster
             r = requests.get(image_url)
             fname = image_url.split('/')[-1]
-            with open(fname, 'wb') as image_file:
-                image_file.write(r.content)
             movie.poster = fname
-            self.safe_add_movie(movie)
+            self.safe_add_movie(movie, movies_widget)
 
-    def safe_add_movie(self, movie):
-        GLib.idle_add(self.add_movie, movie)
+    def safe_add_movie(self, movie, movies_widget):
+        GLib.idle_add(self.add_movie, movie, movies_widget)
 
-    def add_movie(self, movie):
+    def add_movie(self, movie, movies_widget):
         movie_widget = MovieWidget(movie.title, movie.year, movie.poster)
-        print('adding')
-        self.movies_widget.add(movie_widget)
+        movies_widget.add(movie_widget)
+        print(movies_widget == self.discover_movies_widget)
         self.show_all()
 
     def _style():
